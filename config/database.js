59c6d0308a4ee1b7a36fe234fb6a84eef7f6a5af
suitedconnector <tal@trezian.com>
2026-04-08 18:08:27 -0700

    Refactor database connection to use DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -2,15 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } // <-- add this line
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-pool.on('error', (err, client) => {
+pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
 });
